50367116010715ed26d8afbf042067c0e5accd59
Victor Rojas Condori <juandqt@gmail.com>
2015-06-29 17:26:02 +0200

    En clase Project: Añadido Set y Get del atributo manager En clase ManagerRepository: Añadido findBy para usarlo en ProjectService En clase ProjectRepository: Añadido findBy por manager En clase ProjectService: Creado servicio para el Project En ProjectManagementApplication: Añadido imports y el metodo para ejecutar testProject --------------------------------------------------------------------------------------------------------------------------------------------------- En clase Developer: Añadido @ManyToMany del atributo project, añadidos Get y Set En clase Project: Añadido @ManyToMany del atributo developer, añadidos Get y Set En clase DeveloperRepository: Añadido findByName En clase DeveloperService: Añadido un tercer desarrollador En clase ProjectService: Añadido un segundo proyecto. Creado método addDeveloper e introducido en testProject

--- src/main/java/demo/ProjectManagementApplication.java
+++ src/main/java/demo/ProjectManagementApplication.java
@@ -7,8 +7,10 @@ import demo.service.ProjectService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class ProjectManagementApplication {
 
     public static void main(String[] args) {
